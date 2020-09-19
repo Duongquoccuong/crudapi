da33ca681ac2f0ec0a38448b193bde9b7be4b04f
Cường Dương <duongcuongit2k@gmail.com>
2020-09-19 09:24:15 +0700

    first commit

---
+++ src/main/java/product/Product.java
@@ -0,0 +1,63 @@
+package product;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+@Entity
+@Data
+public class Product {
+    private Long id;
+    private String name;
+    private String description;
+    private String manufacturer;
+    private float price;
+
+    public Product(){
+
+    }
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long  getId(){
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+}
